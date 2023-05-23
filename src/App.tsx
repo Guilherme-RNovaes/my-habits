18f3848686a85bde3fc8ac4edc800b0d3511bac2
guilh <guilherme00rogerio@gmail.com>
2023-05-23 20:41:46 -0300

    Configuração inicial do ambiente web

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Habit } from './components/habit'
+import './styles/global.css'
+function App() {
+  return (
+    <div>
+      <Habit completed={10} />
+    </div>
+  )
+}
+
+export default App
